0b0111ea5c2643d53c311122b2a75739acc9b527
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-07-30 16:16:57 -0300

    feat: cria rotas GET para listar todos os produtos e listar produto por id, além de criar rota POST para cadastrar um produto novo

--- src/main/java/com/caio_schultz/inventory_management_system/produto/ProdutoController.java
+++ src/main/java/com/caio_schultz/inventory_management_system/produto/ProdutoController.java
@@ -1,7 +1,10 @@
 package com.caio_schultz.inventory_management_system.produto;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -11,6 +14,40 @@ public class ProdutoController {
 
     public ProdutoController(ProdutoService produtoService) {
         this.produtoService = produtoService;
+    }
+
+
+    @GetMapping("/listar")
+    public ResponseEntity<?> listarProdutos(){
+        List<ProdutoDTO> produtos = produtoService.listarProdutos();
+        if(!produtos.isEmpty()){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(produtos);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não há produtos na lista!");
+        }
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarProdutosPorId(@PathVariable Long id){
+        ProdutoDTO produtoDTO = produtoService.listarProdutoPorId(id);
+        if(produtoDTO != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(produtoDTO);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Produto não encontrado");
+        }
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarProduto(@RequestBody ProdutoDTO produtoDTO){
+        produtoService.criarProduto(produtoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Produto cadastrado com sucesso!");
     }
 
 
--- src/main/java/com/caio_schultz/inventory_management_system/produto/ProdutoService.java
+++ src/main/java/com/caio_schultz/inventory_management_system/produto/ProdutoService.java
@@ -1,13 +1,46 @@
 package com.caio_schultz.inventory_management_system.produto;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProdutoService {
 
     private ProdutoRepository produtoRepository;
+    private ProdutoMapper produtoMapper;
 
-    public ProdutoService(ProdutoRepository produtoRepository) {
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper) {
         this.produtoRepository = produtoRepository;
+        this.produtoMapper = produtoMapper;
     }
+
+    public List<ProdutoDTO> listarProdutos(){
+        List<ProdutoModel> produtos = produtoRepository.findAll();
+        if(!produtos.isEmpty()){
+        return produtos.stream()
+                    .map(produtoMapper::map)
+                    .collect(Collectors.toList());
+        }
+        else{
+            return null;
+        }
+    }
+
+    public ProdutoDTO listarProdutoPorId(Long id){
+        Optional<ProdutoModel> produtoModel = produtoRepository.findById(id);
+        return produtoModel.map(produtoMapper::map).orElse(null);
+    }
+
+    public ProdutoDTO criarProduto(ProdutoDTO produtoDTO){
+        ProdutoModel produtoCriado = produtoRepository.save(produtoMapper.map(produtoDTO));
+        return produtoMapper.map(produtoCriado);
+    }
+
+
+
 }
